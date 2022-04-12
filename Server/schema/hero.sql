7bae74701e85b51ade9b3202931797a0d5ca195c
Mingfeng Li <lmf950122@gmail.com>
2022-04-12 14:04:23 -0700

    Add index to name field

--- Server/schema/hero.sql
+++ Server/schema/hero.sql
@@ -1,5 +1,7 @@
-create table `hero`(
+CREATE TABLE `hero`(
   id int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'PRIMARY KEY', 
   name varchar(25) NOT NULL,
   PRIMARY KEY(`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+
+CREATE INDEX name_index ON hero(name);
